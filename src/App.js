0a81b5dd53d162a635413eb51e6dd8b0a7d156ac
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-08-17 22:00:53 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import HeaderBonito from "./components/Header";
 //PRESIONAN CTRL + SPACE PARA FORZAR LA BUSQUEDA AL IMPORTAR
 import imagen from "./logo.svg";
+import {useState} from "react"
 //CUANDO TU IMPORTAS ALGO QUE NO SEA "JS" DEBERÁS PONERLE SU EXTENSIÓN
 import gato from "./assets/gato2.jpg";
 import FooterBonito from "./components/Footer";
 
 const App = () => {
   //AQUI ESTARÁ TU JS
+  //c
+  const [tareas,setTareas] = useState([])
+  const anadirTareas=()=>{
+    setTareas(["lavar ropa"])
+  }
+
   return (
     //SOLO DEBE TENER UN CONTENEDOR
     //DENTRO DEL RETURN ESTARÁ TODO TU DISEÑO
     //TODOS LOS COMPONENTES QUE IMPORTES DEBEN COMENZAR CON MAYUSCULA
-    <div style={{ backgroundColor: "red" }}>
+    <div  >
+      {tareas}
+      <hr></hr>
+      <button onClick={anadirTareas}>anadirTareas</button>
+      <button ></button>
       <HeaderBonito></HeaderBonito>
       <HeaderBonito />
       <p>HOLA SOY UNA ETIQUETA P</p>
